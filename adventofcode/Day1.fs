322aafbc4dd5b8386aee671032462bb0af61d1a9
dawe <dawedawe@gmx.de>
2019-12-01 17:35:20 +0100

    Day 1 Part 2

--- adventofcode/Day1.fs
+++ adventofcode/Day1.fs
@@ -7,8 +7,22 @@ module Day1 =
 
     let calcFuel mass = mass / 3 - 2
 
+    let calcFuelPart2 mass =
+        let mutable sum = 0
+        let mutable fuel = calcFuel mass
+        while fuel > 0 do
+            sum <- sum + fuel
+            fuel <- calcFuel fuel
+        sum
+
     let day1 () =
         let fuelSum = System.IO.File.ReadAllLines InputFile
                       |> Array.map System.Int32.Parse
                       |> Array.sumBy calcFuel
+        fuelSum
+
+    let day1Part2 () =
+        let fuelSum = System.IO.File.ReadAllLines InputFile
+                      |> Array.map System.Int32.Parse
+                      |> Array.sumBy calcFuelPart2
         fuelSum
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -5,6 +5,6 @@ open Adventofcode
 
 [<EntryPoint>]
 let main argv =
-    let r = Day1.day1 ()
+    let r = Day1.day1Part2 ()
     printfn "%A" r
     0 // return an integer exit code
