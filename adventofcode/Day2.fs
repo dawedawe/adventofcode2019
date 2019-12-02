384d456879a90eb6e03ebc1a28ad330fefb9dc7b
dawe <dawedawe@gmx.de>
2019-12-03 00:32:09 +0100

    Day 2 Part 2

--- adventofcode/Day2.fs
+++ adventofcode/Day2.fs
@@ -9,9 +9,11 @@ module Day2 =
         let input = System.IO.File.ReadAllLines InputFile
         let program = input.[0].Split [|','|]
                       |> Array.map System.Int32.Parse
-        program.[1] <- 12
-        program.[2] <- 2
         program
+    
+    let addNounAndVerb noun verb (program : int []) =
+        program.[1] <- noun
+        program.[2] <- verb
 
     let doOpcode opcodePos inputPos1 inputPos2 outputPos (program : int []) =
         let op = if program.[opcodePos] = 1
@@ -22,8 +24,7 @@ module Day2 =
         let r = (op) program.[program.[inputPos1]] program.[program.[inputPos2]]
         program.[program.[outputPos]] <- r
 
-    let day2 () =
-        let program = getProgram InputFile
+    let runProgram (program : int []) =
         let mutable halt = false
         let mutable opcodePos = 0
         while not halt && opcodePos < (program.Length) do
@@ -32,3 +33,28 @@ module Day2 =
             else doOpcode opcodePos (opcodePos + 1) (opcodePos + 2) (opcodePos + 3) program
             opcodePos <- opcodePos + 4
         program.[0]
+
+    let day2 () =
+        let program = getProgram InputFile
+        addNounAndVerb 12 2 program
+        runProgram program
+
+    let day2Part2 () =
+        let program = getProgram InputFile
+        let mutable r = 0
+        let mutable i = 0
+        let combinations = Array.create (100 * 100) (0, 0)
+        for n in 0 .. 99 do
+            for v in 0 .. 99 do
+                combinations.[i] <- (n, v)
+                i <- i + 1
+        
+        i <- 0
+        let mutable nounverb = (0, 0)
+        while r <> 19690720 do
+            let program' = Array.copy program
+            nounverb <- combinations.[i]
+            addNounAndVerb (fst nounverb) (snd nounverb) program'
+            r <- runProgram program'
+            i <- i + 1
+        100 * (fst nounverb) + (snd nounverb)
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -5,6 +5,6 @@ open Adventofcode
 
 [<EntryPoint>]
 let main argv =
-    let r = Day2.day2 ()
+    let r = Day2.day2Part2 ()
     printfn "%A" r
     0 // return an integer exit code
