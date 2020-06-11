7e7d9239c744632237ad98026bf23ede4711291c
dawe <dawedawe@gmx.de>
2020-06-11 14:58:08 +0200

    Day 22 Part 2

--- adventofcode/Day22.fs
+++ adventofcode/Day22.fs
@@ -37,13 +37,13 @@ module Day22 =
     let increment (cards: int []) n =
         let table = Array.create cards.Length -1
 
-        let rec helper (cards: int []) cardsIdx tableIdx =
+        let rec helper (cards: int []) tableIdx =
             table.[tableIdx] <- cards.[0]
             if (cards.Length > 1)
-            then helper cards.[1..] (cardsIdx + 1) ((tableIdx + n) % table.Length)
+            then helper cards.[1..] ((tableIdx + n) % table.Length)
             else table
 
-        helper cards 0 0
+        helper cards 0
 
     let day22 () =
         let techniques = getTechniques InputFile
@@ -57,3 +57,59 @@ module Day22 =
 
         Array.fold folder cards techniques
         |> Array.findIndex ((=) 2019)
+
+    let part2CardsCount = 119315717514047I
+    let times = 101741582076661I
+
+    let inv (m: bigint) (g: bigint) =
+        let rec fN n i g e l a =
+            match e with
+            | _ when e = 0I -> g
+            | _ ->
+                let o = n / e
+                fN e l a (n - o * e) (i - o * l) (g - o * a)
+
+        (m + (fN m 1I 0I g 0I 1I)) % m
+
+    let (%%%) (i: bigint) (m: bigint) = ((i % m) + m) % m
+
+    let shuffle techniques =
+        let mutable incrementMul = 1I
+        let mutable offsetDiff = 0I
+
+        for tech in techniques do
+            match tech with
+            | DealIntoNewStack ->
+                incrementMul <- (incrementMul * -1I) %%% part2CardsCount
+                offsetDiff <- (offsetDiff + incrementMul) %%% part2CardsCount
+            | Cut n ->
+                offsetDiff <-
+                    (offsetDiff + (bigint n) * incrementMul)
+                    %%% part2CardsCount
+            | Increment n ->
+                incrementMul <-
+                    (incrementMul * inv part2CardsCount (bigint n))
+                    %%% part2CardsCount
+        (incrementMul, offsetDiff)
+
+    let calcIterations (iterations: bigint) (incrementMul: bigint) (offsetDiff: bigint) =
+        let increment =
+            bigint.ModPow(incrementMul, iterations, part2CardsCount)
+
+        let x = (1I - incrementMul) %%% part2CardsCount
+
+        let offset =
+            (offsetDiff
+             * (1I - increment)
+             * (inv part2CardsCount x))
+            %%% part2CardsCount
+
+        (increment, offset)
+
+    let day22Part2 () =
+        let techniques = getTechniques InputFile
+        let position = 2020I
+        let (incmul, offdiff) = shuffle techniques
+        let (increment, offset) = calcIterations times incmul offdiff
+        (offset + position * increment)
+        %%% part2CardsCount
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -5,6 +5,6 @@ open Adventofcode
 
 [<EntryPoint>]
 let main argv =
-    let r = Day22.day22()
+    let r = Day22.day22Part2()
     printfn "%A" r
     0 // return an integer exit code
